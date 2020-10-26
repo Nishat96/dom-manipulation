6b5ce61214fa835bf824bd7707972c763ca44515
Nishat96 <nishatnawar19@gmail.com>
2020-10-26 22:30:10 +0600

    dom manipulating

---
+++ app.js
@@ -0,0 +1,13 @@
+const text = document.querySelector('.title');
+
+const changeColor = document.querySelector('.changeColor');
+
+// text.style.color = 'red';
+
+// text.style.backgroundColor = 'blue';
+
+// text.classList.add('change');
+
+changeColor.addEventListener('click', function(){
+    text.classList.toggle('change');
+})
